889932fde8b69f4361b606b288b9a2b3248cdc7b
Marco Zampetti <m.zampetti@cantierecreativo.net>
2017-11-30 18:34:51 +0100

    merge and fun with irene

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -3,28 +3,20 @@ global.jQuery = $;
 require('slick');
 
 $(document).ready(function(){
-  $(".slick-slider").slick({
-    responsive: [{
-
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        infinite: true
+  $(".homepage-slider").slick({
+    mobileFirst: true,
+    slidesToShow: 1,
+    dots: true,
+    infinite: true,
+    centerMode: true,
+    centerPadding: '0px',
+    responsive: [
+      {
+        breakpoint: 1300,
+        settings: {
+          centerPadding: '100px'
+        }
       }
-
-    }, {
-
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        dots: true
-      }
-
-    }, {
-
-      breakpoint: 300,
-      settings: "unslick" // destroys slick
-
-    }]
+    ]
   });
 });
